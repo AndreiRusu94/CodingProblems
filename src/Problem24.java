fecab4dd3c66aa71a91c2041447b6efb2d3157ea
Andrei Rusu <andrei.rusu2094@gmail.com>
2019-12-15 15:04:08 +0200

    Add solution for Problem24

---
+++ src/Problem24.java
@@ -0,0 +1,48 @@
+/*
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+ */
+
+public class Problem24 {
+    public static void main(String[] args) {
+        Solution24 solution = new Solution24();
+
+        int[] input = {1, 2, 3};
+        ListNode.displayListNode(solution.swapPairs(ListNode.createListNode(input)));
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *      int val;
+ *      ListNode next;
+ *      ListNode(int x) { val = x; }
+ * }
+ */
+class Solution24 {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+
+        while (current.next != null && current.next.next != null) {
+            ListNode firstNode = current.next;
+            ListNode secondNode = current.next.next;
+            firstNode.next = secondNode.next;
+            current.next = secondNode;
+            current.next.next = firstNode;
+            current = current.next.next;
+        }
+
+
+        return dummy.next;
+    }
+}
