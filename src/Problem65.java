a005d97240c35a8ab753e6105fec6c22b569305d
Andrei Rusu <andrei.rusu2094@gmail.com>
2020-04-09 12:17:57 +0300

    Add solution for Problem65

---
+++ src/Problem65.java
@@ -0,0 +1,50 @@
+/*
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+  Example 1:
+
+    Input: [1,2,3]
+    Output: [1,2,4]
+    Explanation: The array represents the integer 123.
+
+  Example 2:
+
+    Input: [4,3,2,1]
+    Output: [4,3,2,2]
+    Explanation: The array represents the integer 4321.
+ */
+
+
+import java.util.Arrays;
+
+public class Problem65 {
+    public static void main(String[] args) {
+        Solution65 solution = new Solution65();
+
+        int[] digits = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(solution.plusOne(digits)));
+    }
+}
+
+class Solution65 {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i]++;
+
+            if (digits[i] / 10 == 0) {
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        int[] newNumber = new int[digits.length + 1];
+        newNumber[0] = 1;
+
+        return newNumber;
+    }
+}
