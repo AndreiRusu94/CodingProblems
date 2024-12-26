529d1c871d451992578ee896e9cc795a23d0495c
Andrei Rusu <andrei.rusu2094@gmail.com>
2024-12-26 12:06:22 +0200

    Added solution for Problem58

---
+++ src/second/Problem58.java
@@ -0,0 +1,46 @@
+package second;
+
+/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal
+substring
+ consisting of non-space characters only.
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ */
+
+public class Problem58 {
+    public static void main(String[] args) {
+        Solution58 sol = new Solution58();
+        System.out.println(sol.lengthOfLastWord("Length of last word is four"));
+    }
+}
+
+class Solution58 {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                return s.length() - 1 - i;
+            }
+        }
+
+        return s.length();
+    }
+}
