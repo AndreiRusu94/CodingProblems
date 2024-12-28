fda8f74ee141ffdde79abaddfa72393a861bbae4
Andrei Rusu <andrei.rusu2094@gmail.com>
2024-12-28 11:12:09 +0200

    Fixed description for Problem392

--- src/second/Problem392.java
+++ src/second/Problem392.java
@@ -1,9 +1,18 @@
 package second;
 
 /*
-Write a function to find the longest common prefix string amongst an array of strings.
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
-If there is no common prefix, return an empty string "".
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
  */
 
 public class Problem392 {
