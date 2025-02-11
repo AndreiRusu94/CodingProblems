f4f6eda0e389f35dace9c2b35684e981b5d65778
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-02-11 18:01:08 +0200

    Added solution for Problem104

---
+++ src/second/TreeNode.java
@@ -0,0 +1,37 @@
+package second;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        this.val = x;
+    }
+
+    public static TreeNode addRecursive(TreeNode current, int value) {
+        if (current == null) {
+            return new TreeNode(value);
+        }
+
+        if (value < current.val) {
+            current.left = addRecursive(current.left, value);
+        } else if (value > current.val) {
+            current.right = addRecursive(current.right, value);
+        } else {
+            return current;
+        }
+
+        return current;
+    }
+
+    public static void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.println(root.val);
+        inorder(root.left);
+        inorder(root.right);
+    }
+}
