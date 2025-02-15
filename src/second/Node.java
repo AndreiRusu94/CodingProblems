67b40cea02edd15c65625886d2d9066de18523a7
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-02-15 11:10:59 +0200

    Added solution for Problem138

---
+++ src/second/Node.java
@@ -0,0 +1,46 @@
+package second;
+
+
+public class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+
+    public static Node createListNode(int[] values) {
+        if (values == null || values.length == 0) {
+            return null;
+        }
+
+        Node current = new Node(values[0]);
+        Node head = current;
+
+        for (int value : values) {
+            current.next = new Node(value);
+            current = current.next;
+        }
+
+        return head.next;
+    }
+
+    public static void outputListNode(Node node) {
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+    }
+
+    public static void displayListNode(Node node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+
+        System.out.println();
+    }
+}
