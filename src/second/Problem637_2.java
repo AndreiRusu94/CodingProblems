4dbde477d62190629e1807d71725c276f73150ac
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-02-16 12:31:43 +0200

    Added solution for Problem637

---
+++ src/second/Problem637_2.java
@@ -0,0 +1,59 @@
+package second;
+/*
+Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
+*/
+
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Problem637_2 {
+    public static void main(String[] args) {
+        Solution637_2 solution = new Solution637_2();
+
+        TreeNode root = TreeNode.addRecursive(null, 1);
+        root = TreeNode.addRecursive(root, 4);
+        root = TreeNode.addRecursive(root, 2);
+        root = TreeNode.addRecursive(root, 3);
+
+        System.out.println(solution.averageOfLevels(root));
+    }
+}
+
+
+class Solution637_2 {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> output = new ArrayList<>();
+
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            double average = 0;
+            int counter = 0;
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                average += current.val;
+                counter++;
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+
+            output.add(average / counter);
+        }
+
+        return output;
+    }
+}
+
+
