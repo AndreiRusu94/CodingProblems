7a5b0406c42a67dd29d1caa2dc988c06352a9be6
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-02-16 11:22:42 +0200

    Added solution for Problem112

---
+++ src/second/Problem112.java
@@ -0,0 +1,49 @@
+package second;
+/*
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+Note: A leaf is a node with no children.
+
+  Example:
+
+    Given the below binary tree and sum = 22,
+
+          5
+         / \
+        4   8
+       /   / \
+      11  13  4
+     /  \      \
+    7    2      1
+    return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+*/
+
+public class Problem112 {
+    public static void main(String[] args) {
+        Solution112 solution = new Solution112();
+
+        TreeNode tree = TreeNode.addRecursive(null, 1);
+        tree = TreeNode.addRecursive(tree, 4);
+        tree = TreeNode.addRecursive(tree, 2);
+        tree = TreeNode.addRecursive(tree, 3);
+        tree = TreeNode.addRecursive(tree, 5);
+        tree = TreeNode.addRecursive(tree, 6);
+        tree = TreeNode.addRecursive(tree, 1);
+
+        System.out.println(solution.hasPathSum(tree, 10));
+    }
+}
+
+class Solution112 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
