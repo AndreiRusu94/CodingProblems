cf8078faccce9e7676c0bd12c9a7fb43ad477a34
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-03-02 15:59:58 +0200

    Added solution for Problem918

---
+++ src/second/Problem918.java
@@ -0,0 +1,52 @@
+package second;
+/*
+Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
+
+A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
+
+A subarray may only include each element of the fixed buffer nums at most once. Formally, for a subarray nums[i], nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
+
+Example 1:
+
+Input: nums = [1,-2,3,-2]
+Output: 3
+Explanation: Subarray [3] has maximum sum 3.
+Example 2:
+
+Input: nums = [5,-3,5]
+Output: 10
+Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10.
+Example 3:
+
+Input: nums = [-3,-2,-3]
+Output: -2
+Explanation: Subarray [-2] has maximum sum -2.
+ */
+
+public class Problem918 {
+    public static void main(String[] args) {
+        Solution918 solution = new Solution918();
+
+        int[] nums = {5, -3, 5};
+
+        System.out.println(solution.maxSubarraySumCircular(nums));
+    }
+}
+
+class Solution918 {
+    public int maxSubarraySumCircular(int[] nums) {
+        int total = 0;
+        int currentMin = 0;
+        int currentMax = 0;
+        int max = nums[0];
+        int min = nums[0];
+        for (int num : nums) {
+            total += num;
+            currentMin = Math.min(currentMin + num, num);
+            currentMax = Math.max(currentMax + num, num);
+            min = Math.min(currentMin, min);
+            max = Math.max(currentMax, max);
+        }
+        return max < 0 ? max : Math.max(max, total - min);
+    }
+}
