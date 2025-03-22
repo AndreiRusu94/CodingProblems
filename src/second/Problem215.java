3da4e3e5db17d446545a64a21e8e16f31cbf8042
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-03-22 17:37:45 +0200

    Added solution for Problem215

---
+++ src/second/Problem215.java
@@ -0,0 +1,50 @@
+package second;
+
+/*
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Can you solve it without sorting?
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+*/
+
+
+import java.util.PriorityQueue;
+
+public class Problem215 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5};
+
+        Solution215 solution215 = new Solution215();
+        System.out.println("Kth largest element in array: " + solution215.findKthLargest(nums, 2));
+    }
+}
+
+class Solution215 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i < k; i++) {
+            minHeap.offer(nums[i]);
+        }
+
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.offer(nums[i]);
+            }
+        }
+
+        return minHeap.peek();
+    }
+}
