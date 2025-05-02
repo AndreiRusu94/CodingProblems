165fe6dfc545073aef8648b9f052b1b49974d09e
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-05-02 11:18:07 +0300

    Added solution for Problem605

---
+++ src/second/Problem605.java
@@ -0,0 +1,56 @@
+package second;
+/*
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+
+
+Constraints:
+
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
+ */
+
+public class Problem605 {
+    public static void main(String[] args) {
+        Solution605 solution = new Solution605();
+
+        System.out.println(solution.canPlaceFlowers(new int[]{1, 0, 0, 0, 0, 1, 1, 0, 1}, 2));
+    }
+}
+
+class Solution605 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 1) {
+                continue;
+            }
+
+            if ((i ==0 || flowerbed[i - 1] != 1) && ((i == flowerbed.length - 1) || flowerbed[i + 1] != 1)) {
+                flowerbed[i++] = 1;
+                n--;
+
+                if (n == 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
