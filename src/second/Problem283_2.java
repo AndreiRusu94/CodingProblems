966d2e80dc4f543f9a32e8a359ff1594ecba9bb5
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-05-03 11:31:28 +0300

    Added solution for Problem283 (O(n) vs O(2n))

---
+++ src/second/Problem283_2.java
@@ -0,0 +1,44 @@
+package second;
+
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ */
+
+import java.util.Arrays;
+
+public class Problem283_2 {
+    public static void main(String[] args) {
+        Solution283_2 sol = new Solution283_2();
+        int[] nums = {0, 1, 0, 3, 12};
+        sol.moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
+
+class Solution283_2 {
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0 && nums[slow] == 0) {
+                int temp = nums[fast];
+                nums[fast] = nums[slow];
+                nums[slow] = temp;
+            }
+
+            if (nums[slow] != 0) {
+                slow++;
+            }
+        }
+    }
+}
