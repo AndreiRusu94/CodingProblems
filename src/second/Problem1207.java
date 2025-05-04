b33156ef9c302bdfdfc9cea82b6ecc9a8ade9c77
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-05-04 12:58:52 +0300

    Added solution for Problem1657

--- src/second/Problem1207.java
+++ src/second/Problem1207.java
@@ -1,26 +1,21 @@
 package second;
 
 /*
-Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
-
-answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
-answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
-Note that the integers in the lists may be returned in any order.
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
 
 Example 1:
 
-Input: nums1 = [1,2,3], nums2 = [2,4,6]
-Output: [[1,3],[4,6]]
-Explanation:
-For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
-For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums1. Therefore, answer[1] = [4,6].
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
 Example 2:
 
-Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
-Output: [[3],[]]
-Explanation:
-For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
-Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
  */
 
 import java.util.HashMap;
