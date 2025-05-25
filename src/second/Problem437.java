5d34308820ce4fe5259ae1a207febb1c9e78ea3a
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-05-25 18:04:37 +0300

    Added solution for Problem437

---
+++ src/second/Problem437.java
@@ -0,0 +1,65 @@
+package second;
+/*
+Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+
+Example 1:
+
+
+Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+Output: 3
+Explanation: The paths that sum to 8 are shown.
+Example 2:
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: 3
+ */
+
+import java.util.HashMap;
+
+public class Problem437 {
+    public static void main(String[] args) {
+        Solution437 solution = new Solution437();
+
+        TreeNode tree = TreeNode.addRecursive(null, 1);
+        tree = TreeNode.addRecursive(tree, 4);
+        tree = TreeNode.addRecursive(tree, 2);
+        tree = TreeNode.addRecursive(tree, 3);
+        tree = TreeNode.addRecursive(tree, 5);
+        tree = TreeNode.addRecursive(tree, 6);
+        tree = TreeNode.addRecursive(tree, 1);
+
+        System.out.println(solution.pathSum(tree, 7));
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution437 {
+    public int pathSum(TreeNode root, int sum) {
+        HashMap<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+        return helper(root, 0, sum, preSum);
+    }
+
+    public int helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        currSum += root.val;
+        int res = preSum.getOrDefault(currSum - target, 0);
+        preSum.put(currSum, preSum.getOrDefault(currSum, 0) + 1);
+
+        res += helper(root.left, currSum, target, preSum) + helper(root.right, currSum, target, preSum);
+        preSum.put(currSum, preSum.get(currSum) - 1);
+        return res;
+    }
+}
