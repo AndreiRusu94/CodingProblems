c1e2f65520ecdb7d7bf9e7624e7d2862bff618ee
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-06-02 11:13:55 +0300

    Added solution for Problem450

---
+++ src/second/Problem450.java
@@ -0,0 +1,81 @@
+package second;
+/*
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+
+Example 1:
+
+Input: root = [5,3,6,2,4,null,7], key = 3
+Output: [5,4,6,2,null,null,7]
+Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+Example 2:
+
+Input: root = [5,3,6,2,4,null,7], key = 0
+Output: [5,3,6,2,4,null,7]
+Explanation: The tree does not contain a node with value = 0.
+*/
+
+public class Problem450 {
+    public static void main(String[] args) {
+        Solution450 solution = new Solution450();
+
+        TreeNode root = TreeNode.addRecursive(null, 1);
+        root = TreeNode.addRecursive(root, 4);
+        root = TreeNode.addRecursive(root, 2);
+        root = TreeNode.addRecursive(root, 3);
+
+        System.out.println(solution.deleteNode(root, 2));
+    }
+}
+
+
+class Solution450 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            if (root.left == null) {
+                return root.right;
+            }
+
+            if (root.right == null) {
+                return root.left;
+            }
+
+            TreeNode inorderSuccessor = getInorderSuccessor(root.right);
+            root.val = inorderSuccessor.val;
+
+            root.right = deleteNode(root.right, inorderSuccessor.val);
+
+        }
+
+        return root;
+    }
+
+    private TreeNode getInorderSuccessor(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+
+        return root;
+    }
+}
+
+
