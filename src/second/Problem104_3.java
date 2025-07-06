ea1bbe1ce6dcf1652a9254c4d21a23c12efdb357
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-07-06 18:46:21 +0300

    Added solution for Problem104

---
+++ src/second/Problem104_3.java
@@ -0,0 +1,40 @@
+package second;
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+  Example:
+
+    Given binary tree [3,9,20,null,null,15,7],
+
+        3
+       / \
+      9  20
+        /  \
+       15   7
+    return its depth = 3.
+*/
+
+public class Problem104_3 {
+    public static void main(String[] args) {
+        Solution104_3 solution = new Solution104_3();
+
+        TreeNode tree = TreeNode.addRecursive(null, 1);
+        tree = TreeNode.addRecursive(tree, 4);
+        tree = TreeNode.addRecursive(tree, 2);
+        tree = TreeNode.addRecursive(tree, 3);
+        tree = TreeNode.addRecursive(tree, 5);
+        tree = TreeNode.addRecursive(tree, 6);
+
+        System.out.println(solution.maxDepth(tree));
+    }
+}
+
+class Solution104_3 {
+    public int maxDepth(TreeNode root) {
+        return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
