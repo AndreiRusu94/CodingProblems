705d2ca5efb69d8612a1b79c3d4d0e48a4fbccb0
Andrei Rusu <andrei.rusu2094@gmail.com>
2025-07-13 14:02:31 +0300

    Added solution for Problem101

---
+++ src/third/Problem101.java
@@ -0,0 +1,63 @@
+package third;
+/*
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+    For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+        1
+       / \
+      2   2
+     / \ / \
+    3  4 4  3
+
+
+    But the following [1,2,2,null,3,null,3] is not:
+
+        1
+       / \
+      2   2
+       \   \
+       3    3
+*/
+
+public class Problem101 {
+    public static void main(String[] args) {
+        Solution101 solution = new Solution101();
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println("Is symmetric: " + solution.isSymmetric(root));
+    }
+}
+
+class Solution101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+}
