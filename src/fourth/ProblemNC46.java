d1994cac21bf784ffb4685b911c13ee897d1eaf8
Andrei Rusu <andrei.rusu2094@gmail.com>
2026-05-14 23:14:18 +0300

    Added solution for ProblemNC46 (Permutation in String)

---
+++ src/fourth/ProblemNC46.java
@@ -0,0 +1,83 @@
+package fourth;
+/*
+You are given two strings s1 and s2.
+
+Return true if s2 contains a permutation of s1, or false otherwise. That means if a permutation of s1 exists as a substring of s2, then return true.
+
+Both strings only contain lowercase letters.
+
+Example 1:
+
+Input: s1 = "abc", s2 = "lecabee"
+
+Output: true
+Explanation: The substring "cab" is a permutation of "abc" and is present in "lecabee".
+
+Example 2:
+
+Input: s1 = "abc", s2 = "lecaabee"
+
+Output: false
+*/
+
+public class ProblemNC46 {
+    public static void main(String[] args) {
+        SolutionNC46 solution = new SolutionNC46();
+        System.out.println(solution.checkInclusion("abc", "lecabee"));
+    }
+}
+
+class SolutionNC46 {
+
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']++;
+        }
+
+        int matches = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1Count[i] == s2Count[i]) {
+                matches++;
+            }
+        }
+
+        int l = 0;
+        for (int r = s1.length(); r < s2.length(); r++) {
+            if (matches == 26) {
+                return true;
+            }
+
+            int index = s2.charAt(r) - 'a';
+            s2Count[index]++;
+
+            if (s1Count[index] == s2Count[index]) {
+                matches++;
+            } else if (s1Count[index] + 1 == s2Count[index]) {
+                matches--;
+            }
+
+            index = s2.charAt(l) - 'a';
+            s2Count[index]--;
+
+            if (s1Count[index] == s2Count[index]) {
+                matches++;
+            } else if (s1Count[index] - 1 == s2Count[index]) {
+                matches--;
+            }
+
+            l++;
+        }
+
+        return matches == 26;
+    }
+
+}
+
